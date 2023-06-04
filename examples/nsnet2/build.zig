b5c5d3437eafe3b0f6ed090269936a43581b63fb
recursiveGecko <1461520+recursiveGecko@users.noreply.github.com>
2023-06-05 00:07:12 +0200

    Add NSNet2

--- examples/nsnet2/build.zig
+++ examples/nsnet2/build.zig
@@ -7,15 +7,16 @@ pub fn build(
     examples_step: *std.Build.Step,
 ) !void {
     const exe = b.addExecutable(.{
-        .name = "silero_vad",
+        .name = "nsnet2",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
         .root_source_file = .{ .path = projectPath("src/main.zig") },
         .target = common_options.target,
         .optimize = common_options.optimize,
     });
-    
+
     exe.linkSystemLibrary("sndfile");
+    try addKissFFT(b, exe, common_options);
     try main_build.linkPackage(b, exe, common_options);
 
     const exe_install = b.addInstallArtifact(exe);
@@ -28,8 +29,33 @@ pub fn build(
         run_cmd.addArgs(args);
     }
 
-    const run_step = b.step("run-silero-vad", "Run the Silero VAD example");
+    const run_step = b.step("run-nsnet", "Run the NSNet2 example");
     run_step.dependOn(&run_cmd.step);
+}
+
+fn addKissFFT(
+    b: *std.Build,
+    exe: *std.Build.Step.Compile,
+    options: main_build.CommonOptions,
+) !void {
+    const source_files: []const []const u8 = &.{
+        projectPath("lib/kissfft/kiss_fft.c"),
+        projectPath("lib/kissfft/kiss_fftr.c"),
+    };
+
+    const lib = b.addStaticLibrary(.{
+        .name = "kissfft",
+        .optimize = options.optimize,
+        .target = options.target,
+    });
+
+    lib.linkLibC();
+    lib.addCSourceFiles(source_files, &.{"-Wall"});
+
+    lib.defineCMacro("kiss_fft_scalar", "float");
+
+    exe.addIncludePath(projectPath("lib/kissfft"));
+    exe.linkLibrary(lib);
 }
 
 pub inline fn projectPath(path: []const u8) []const u8 {
